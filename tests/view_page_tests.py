9132cb79a3fb5c0e9c2e15b956d284acffa6e4f7
vleseg <vlesiil@yandex.ru>
2015-02-19 21:34:11 +0600

    Version timestamp on view article page test

--- tests/view_page_tests.py
+++ tests/view_page_tests.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from datetime import datetime
+# Internal project imports
 from base import BaseTestCase
 
 
@@ -62,3 +64,26 @@ class BasicViewPageTest(BaseTestCase):
         self.assertHasLink(
             new_article, '#history-link', text='History',
             href='/_history/kittens')
+
+
+class TimestampTest(BaseTestCase):
+    def test_there_is_a_timestamp_of_currently_viewed_version(self):
+        # TODO: extract article creating logic as a method of BaseTestCase
+        # TODO: create_article(url, logout=False)
+        # TODO: replace edit_form with edit_page everywhere
+        # Bob signs up and immediately creates a new article.
+        self.sign_up()
+        edit_page = self.testapp.get('/what_does_the_fox_say').follow()
+
+        # He saves the article with the default values. He is redirected to the
+        # article.
+        created = datetime.utcnow()
+        new_article = self.fill_form(edit_page).submit().follow()
+
+        # There's a timestamp on the page, which indicates date and time when
+        # currently current version (the first one) was created.
+        timestamp = new_article.pyquery('#ts-created')
+        self.assertTrue(timestamp.text().startswith('Version of '))
+
+    def test_timestamp_of_currently_viewed_version_is_correct(self):
+        pass
